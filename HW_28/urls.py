33824e6a86937c609c38cc9a206a7ba5c94e8a1f
Harry <vin-ig@yandex.ru>
2022-08-22 22:59:08 +0300

    Image upload fixed

--- HW_28/urls.py
+++ HW_28/urls.py
@@ -13,15 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 import ad.views
+from HW_28 import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', ad.views.IndexView.as_view()),
-    path('ad/', include('ad.urls')),
-    path('cat/', include('category.urls')),
-    path('user/', include('user.urls')),
+	path('admin/', admin.site.urls),
+	path('', ad.views.IndexView.as_view()),
+	path('ad/', include('ad.urls')),
+	path('cat/', include('category.urls')),
+	path('user/', include('user.urls')),
 ]
+
+if settings.DEBUG:
+	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- ad/urls.py
+++ ad/urls.py
@@ -9,9 +9,6 @@ urlpatterns = [
 	path('<int:pk>/', views.AdDetailView.as_view(), name='ad_detail'),
 	path('create/', views.AdCreateView.as_view(), name='ad_create'),
 	path('<int:pk>/update/', views.AdUpdateView.as_view(), name='ad_update'),
-	path('<int:pk>/image/', views.AdImageView.as_view(), name='ad_image'),
+	path('<int:pk>/upload_image/', views.AdImageView.as_view(), name='ad_image'),
 	path('<int:pk>/delete/', views.AdDeleteView.as_view(), name='ad_delete'),
 ]
-
-if settings.DEBUG:
-	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
